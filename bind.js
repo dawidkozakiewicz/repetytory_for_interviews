f3dfb1d62d88e52e26b44fec992c212ecab17ddf
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-06 23:03:16 +0200

    more bind

--- bind.js
+++ bind.js
@@ -1,28 +1,38 @@
-function testX() {
-    console.log(this); //window
-}
-testX();
+// 1. Chciałem wywołać bind() na obiekcie, posiadającym metodę:
 
-const ob = {
-    name: "pies",
-
-    show() {
-        console.log(this); //ob
-
-        //poniższa funkcja NIE JEST metodą obiektu ob
-        //ani metodą poniższego obiektu obj
-        function testY() {
-            console.log(this); //window
-        }
-        testY();
-
-        const obj = {
-            show() {
-                console.log(this); //obj
-            }
-        }
-        obj.show();
+const someObj = {
+    someStr: "",
+    addChar(letter) {
+        this.someStr += letter
     }
 }
 
-ob.show();
+// someObj.addChar("Q") <----- tak bym musiał robić, gdybym nie użył bind()
+// someObj.addChar("u") <-----         
+// someObj.addChar("e") <-----
+// someObj.addChar("e") <-----
+// someObj.addChar("n") <-----
+
+const addCh = someObj.addChar.bind(someObj) // tu wydobywam metodę z obiektu (someObj.addChar), potem przy pomocy bind(someObj) powiązuję ją
+// z kontekstem tegoż obiektu (teraz już this === someObj). Od tej pory działam skrótowo:
+addCh("Q")                                  // <---
+addCh("u")                                  // <---
+addCh("e")                                  // <---
+addCh("e")                                  // <---
+addCh("n")                                  // <---
+
+console.log(someObj.someStr)
+
+// 2. A tak rozumiem Twój przykład z fiszki:
+
+const numbers = [1, 2];
+const addFn = numbers.push.bind(numbers); // pobieramy z tablicy "numbers" jej natywną metodę (natywną dla wszystkich tablic) jaką jest push(). 
+// Następnie wykonujemy na niej funkcję bind(numbers) i jako argument wstawiamy ją samą.
+// Dzięki temu pushowanie odbywało się będzie właśnie na niej bo od tej pory this === numbers:
+addFn(3);
+addFn(4)
+
+console.log(numbers); // [1, 2, 3, 4]
+
+
+
