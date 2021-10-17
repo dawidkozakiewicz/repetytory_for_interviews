fb049c4ca63d31677b5080e0d1ddd0edcf5cee04
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-17 23:15:51 +0200

    prototype chaining

--- temporary.js
+++ temporary.js
@@ -1,9 +1,25 @@
-const cat = {
-    name: "Mruczuś"
+function Enemy(name, x, y) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    console.log(`Tworzę przeciwnika: ${this.x}x${this.y}`);
 }
 
-const cat2 = cat.toString()
-
-function Janush(nose) {
-    this.nose = nose
+Enemy.prototype.fly = function () {
+    return this.name + " lecę";
 }
+
+function EnemyShoot(name, x, y) {
+    this.name = name;
+    this.x = x;
+    this.y = y;
+    this.type = "shooter";
+}
+
+EnemyShoot.prototype.shoot = function () {
+    return this.name + " strzelam";
+}
+
+const enemyS = new EnemyShoot("Shooter", 20, 20);
+console.log(enemyS.shoot()); //Shooter strzelam
+console.log(enemyS.__prototype__)
