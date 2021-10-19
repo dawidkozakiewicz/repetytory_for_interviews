d06923acbca70dd551518c8e3ff407b051a4ccdd
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-19 23:46:58 +0200

    deepr in constructors

--- temporary.js
+++ temporary.js
@@ -1,34 +1,16 @@
-function Enemy(name, x, y) {
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    console.log(`Tworzę przeciwnika: ${this.x}x${this.y}`);
+function Enemy(speed, power) {
+    this.live = 3;
+    this.speed = speed;
+    this.power = power;
+
+    this.print = function () {
+        console.log(`
+            Przeciwnik ma:
+            życia: ${this.live}
+            szybkości: ${this.speed}
+            siły ataku: ${this.power}
+        `);
+    }
 }
 
-Enemy.prototype.fly = function () {
-    return this.name + " lecę";
-}
-
-function EnemyShoot(name, x, y) {
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    this.type = "shooter";
-    console.log(`Tworzę przeciwnika: ${this.x}x${this.y}`);
-
-}
-
-EnemyShoot.prototype = Object.create(Enemy.prototype);
-
-EnemyShoot.prototype.shoot = function () {
-    return this.name + " strzelam";
-}
-
-
-const enemyN = new Enemy("Normalny", 10, 15);
-console.log(enemyN.fly()); //Normalny latam
-// console.log(enemyN.shoot()); błąd - nie ma takiej metody
-
-const enemyS = new EnemyShoot("Shooter", 10, 20);
-console.log(enemyS.fly()); //Shooter latam
-console.log(enemyS.shoot()); //Shooter strzelam
+console.dir(Enemy);
