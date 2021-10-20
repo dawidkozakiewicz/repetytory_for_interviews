34c98269d27070cab88d84e65ee6e06a25a319f4
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-20 23:45:10 +0200

    my own examples

--- temporary.js
+++ temporary.js
@@ -1,16 +1,26 @@
-function Enemy(speed, power) {
-    this.live = 3;
-    this.speed = speed;
-    this.power = power;
-
-    this.print = function () {
-        console.log(`
-            Przeciwnik ma:
-            życia: ${this.live}
-            szybkości: ${this.speed}
-            siły ataku: ${this.power}
-        `);
-    }
+function CreateObj(name, age) {
+    this.name = name,
+        this.age = age
 }
 
-console.dir(Enemy);
+
+
+
+
+function CreateObj2(speed) {
+    this.speed = speed
+
+}
+CreateObj.prototype = Object.create(CreateObj2.prototype)
+CreateObj.prototype.constructor = CreateObj
+
+CreateObj.prototype.show = function () {
+    console.log(this.name)
+}
+
+const me = new CreateObj("Dawid", 42)
+console.log(me)
+me.show()
+
+const he = new CreateObj2(220)
+console.log(he)
