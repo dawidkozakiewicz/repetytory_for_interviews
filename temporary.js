ed3ea8942a6454adac4669354b429626f7dfa886
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-22 23:09:39 +0200

    Anon level 42

--- temporary.js
+++ temporary.js
@@ -16,7 +16,7 @@ CreateObj.prototype.constructor = CreateObj
 
 CreateObj.prototype.show = function () {
     console.log(this.name)
-}
+} // do zbadania, dlaczego ta linijka musiała znaleźć się poniżej wiersza 14, bo inaczej kod nie działa
 
 const me = new CreateObj("Dawid", 42)
 console.log(me)
