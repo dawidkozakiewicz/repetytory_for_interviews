13557bb56a96b9ab30534cff44d9878a89783bd6
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-10-27 22:49:11 +0200

    definition

---
+++ strict.js
@@ -0,0 +1,4 @@
+// Dyrektywa „use strict” była nowością w ECMAScript w wersji 5.
+// Nie jest to instrukcja, ale wyrażenie dosłowne, ignorowane przez wcześniejsze wersje JavaScript. 
+// Celem "use strict" jest wskazanie, że kod powinien być wykonywany w "strict mode".
+// W trybie ścisłym nie można na przykład używać niezadeklarowanych zmiennych.
