8e1fc5113ef9f9cffe4019eaad569c0b783907ed
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-11-02 23:17:53 +0100

    namespaces my own reflexions

---
+++ namespaces2/index2.js
@@ -0,0 +1,5 @@
+(function () {
+    document.querySelector('.changeColor').addEventListener("mouseout", function () {
+        document.querySelector('.changeColor').style.backgroundColor = 'yellow';
+    })
+})()
---
+++ namespaces2/script.js
@@ -0,0 +1,8 @@
+(function () {
+    document.querySelector('.changeColor').addEventListener("mouseover", function () {
+        document.querySelector('.changeColor').style.backgroundColor = 'purple';
+    })
+})()
+
+
+
