f1f3210b86fd62c9e12f4d89b63a77ed8b6282a6
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-11-03 22:31:07 +0100

    DOM and namespaces

---
+++ namespaces copy/namespace2.js
@@ -0,0 +1,14 @@
+let divA = document.getElementById('output');
+
+function colorDiv() {
+
+    divA.style.backgroundColor = 'olive';
+    divA.style.color = '#333';
+}
+
+
+divA.addEventListener('mouseover', this.colorDiv)
+
+
+
+
