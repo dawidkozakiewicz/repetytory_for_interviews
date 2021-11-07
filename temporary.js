cfe31049a96b5dbfbb0404aaa04a1fe2f2b1bea2
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-11-07 23:55:28 +0100

    temporary changes

--- temporary.js
+++ temporary.js
@@ -1,26 +1,8 @@
-function CreateObj(name, age) {
-    this.name = name,
-        this.age = age
-}
+const tab = [1,2,3,4,5]
 
+const newTab = tab.forEach((numb)=>{
+ 
+    return numb * numb
+})
 
-
-
-
-function CreateObj2(speed) {
-    this.speed = speed
-
-}
-CreateObj.prototype = Object.create(CreateObj2.prototype)
-CreateObj.prototype.constructor = CreateObj
-
-CreateObj.prototype.show = function () {
-    console.log(this.name)
-} // do zbadania, dlaczego ta linijka musiała znaleźć się poniżej wiersza 14, bo inaczej kod nie działa
-
-const me = new CreateObj("Dawid", 42)
-console.log(me)
-me.show()
-
-const he = new CreateObj2(220)
-console.log(he)
+console.log(newTab)
