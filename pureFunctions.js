c19e7eb198cd4c66fd4b8f88ae8349aae6f701ad
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-11-10 22:43:06 +0100

    pure functions2

---
+++ pureFunctions.js
@@ -0,0 +1,3 @@
+// Pure Function to funkcja (blok kodu), która zawsze zwraca ten sam wynik, jeśli przekazano te same argumenty.
+// Nie zależy to od żadnego stanu ani zmiany danych podczas wykonywania programu,
+// a jedynie od jego argumentów wejściowych.
