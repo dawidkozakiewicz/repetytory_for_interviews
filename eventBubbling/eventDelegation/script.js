bafb21c6ac9a1ba0446e014ff972ee057175eac6
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-11-18 21:33:08 +0100

    event delegation2

--- eventBubbling/eventDelegation/script.js
+++ eventBubbling/eventDelegation/script.js
@@ -2,3 +2,10 @@ const form = document.querySelector("#signin");
 form.addEventListener('click', (event)=>{
     console.log(event.target)
 })
+
+
+const form2 = document.querySelector("#signin2");
+form2.addEventListener('input', (event)=>{
+    console.log(event.target)
+})
+
