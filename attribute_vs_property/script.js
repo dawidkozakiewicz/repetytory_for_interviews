61a273ac30d1216d3cb0cc395f5d388c708216fa
Dawid Kozakiewicz <turgor1996@gmail.com>
2021-12-03 10:53:00 +0100

    at vs prop

---
+++ attribute_vs_property/script.js
@@ -0,0 +1,3 @@
+const input = document.querySelector('input')
+console.log(input.value) //value z pozycji DOM jest właściwością
+input.setAttribute('value', 100) // to zmieni w debbugerze przeglądarki atrybut w HTML
