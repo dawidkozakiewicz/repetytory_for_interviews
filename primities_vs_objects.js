91ef8ebea218e94841a8fd99c49b20382fa08175
Dawid Kozakiewicz <turgor1996@gmail.com>
2022-01-14 23:05:30 +0100

    primitives vs objects

--- primities_vs_objects.js
+++ primities_vs_objects.js
@@ -5,3 +5,13 @@ a = 15;
 
 console.log(a); //15
 console.log(b); //12
+
+// First, let’s define what are primitive types.
+
+// Primitive types in JavaScript are
+
+// strings
+// numbers (Number and BigInt)
+// booleans (true or false)
+// undefined
+// Symbol values
