0bedcaa27ec3aa32478fedc606b0600fc27f73a5
Dawid Kozakiewicz <turgor1996@gmail.com>
2022-01-19 22:24:42 +0100

    shallow and deep copy2

--- shallow_deep_copy.js
+++ shallow_deep_copy.js
@@ -1,3 +1,7 @@
+// Metodami (spread i assign) możemy skopiować tylko płaskie obiekty. 
+// Jeżeli któraś z właściwości wskazuje na inny obiekt,
+// zostanie skopiowana tylko referencja do tego obiektu:
+
 const food = { type : "rybki" }
 
 const a = {
@@ -6,8 +10,9 @@ const a = {
 }
 
 const b = { ...a }
-b.food.type = "chrupki";
+b.food.type = "chrupki"; //nadpisało w obydwu obiektach (a i b) zamiast w jednym
 
+console.log(a)
 console.log(b)
 
 console.log(b.food, a.food); //{type : "chrupki"}, {type : "chrupki"}
