75e5777953833b086ced10b7aafb2d18465a769a
Dawid Kozakiewicz <turgor1996@gmail.com>
2022-01-21 20:22:25 +0100

    shallow and deep copy3

--- shallow_deep_copy.js
+++ shallow_deep_copy.js
@@ -16,3 +16,40 @@ console.log(a)
 console.log(b)
 
 console.log(b.food, a.food); //{type : "chrupki"}, {type : "chrupki"}
+
+// assign
+
+const aa = {
+    name : "kot",
+    age: 7,
+    speed: 10
+}
+
+const bb = {
+    name : "pies",
+    age : 5
+}
+
+const c = Object.assign({}, aa, bb);
+
+console.log(c); //{name : "pies", age: 5, speed: 10}
+
+// głęboka kopia
+
+// Żeby sklonować obiekt głęboko, musimy zastosować inne techniki - np. skorzystać z obiektu JSON
+
+const ob = {
+    name : "Marcin",
+    pet : {
+        name : "Feliks",
+        kind : "cat"
+    }
+}
+
+const ob2 = JSON.parse(JSON.stringify(ob));
+
+ob2.pet.name = "Super Szamson";
+ob2.pet.kind = "pies";
+
+console.log(ob.pet.name, ob2.pet.name); //Feliks, Super Szamson
+console.log(ob.pet.kind, ob2.pet.kind); //cat, pies
